e35c31efa4390fb2f0c0e99361d6ad018c49bd6e
frank <guanqi@ualberta.ca>
2014-11-12 17:19:18 -0700

    added stopwords

--- parser.py
+++ parser.py
@@ -1,57 +1,88 @@
 #!/usr/bin/python
-import errors
-import lines
 import sys, getopt, exceptions
+import nltk
 
 def processStopWords(text):
     stopWordsList = []
     for line in text:
         stopWordsList.extend(line.strip('\n').split(' '))
+    return stopWordsList
 
-def processRawData(text):
-
+def processRawData(text, stopWords = None):
+    rawDataList = []
+    for line in text:
+        line.split(' ')
+    return rawDataList
 
 def readArgsFromInput(argv):
-    rawDataName,stopWordsName,outputName = None
+    rawDataName = stopWordsName = outputName = directory = None 
+    # Record the flags that user are using
+    usedOpts = []
     try:
         opts, args = getopt.getopt(argv,"hr:s:i1o",["rawdata=","stopwords=","ouput="])
     except getopt.GetoptError:
         print 'run parser.py with flag -h to get help doc' 
-        sys.exit(2)
+        sys.exit()
     for opt, arg in opts:
         if opt == '-h':
             print 'Flags List:'
-            print '-h Help'
-            print '-r <rawdata_filename> Import rawdata' 
-            print '-s <stopwords_filename> Import stopwords'
-            print '-o <outputfile_filename> Export results' 
+            print '-h Help' 
+            print '-r <rawdata_filename> Import raw data file' 
+            print '-s <stopwords_filename> Import stopwords file'
+            print "-o <outputfile_filename> Define the name of output file.(It's named as no_stopwords.txt by default)"
             print '-1 Mode 1: Process raw data without stripping the stopwords'
+            sys.exit()
+
         elif opt in ("-r","--rawdata"):
             rawDataName = arg
+            usedOpts.append('-r')
+
         elif opt in ("-s","--stopwords"):
             stopWordsName = arg
+            usedOpts.append('-s')
+
         elif opt in ("-o","--output"):
             outputName = arg
+            usedOpts.append('-o')
+
         elif opt in ("-i"):
+            usedOpts.append('-i')
             # TODO:export stats images
             pass
         elif opt in ("-1"):
-            # TODO:Mode 1 -- export
+            usedOpts.append('-1')
+            # TODO:Mode 1 -- Process raw data without stripping the stopwords
             pass 
-        else
-            sys.exit(2)
+        else:
+            sys.exit("Invalid Flag(s)")
 
-    return rawDataName, stopWordsName, outputName, opts
+    # pareser wihout flag -o, then the output will be named as this by default
+    if '-o' not in usedOpts:
+        outputName = 'no_stopwords.txt'
+
+    # cannot proceed without filename of raw data
+    if rawDataName == None:
+        sys.exit("Cannot proceed wihout the filename of raw data")
+
+    return rawDataName, stopWordsName, outputName, usedOpts
 
 def main(argv):
+    
+    rawDataName, stopWordsName, outputName, usedOpts = readArgsFromInput(argv)
+    # load raw data
     try:
-        rawDataName, stopWordsName, outputName, opts = readArgsFromInput(argv)
-        rawData = open(rawDataName,'r')
-        stopWords = open(stopWordsName,'r')
+        rawDataFile = open(rawDataName,'r')
+        
+        # parser is in mode 1 then prcoess raw data without stripping the stopwords
+        if '-1' in usedOpts:
+            #processRawData(rawDataFile)
+        else:
+            stopWordsFile = open(stopWordsName,'r')
+            stopWordsList = processStopWords(stopWordsFile)
+            #processRawData(rawDataFile,stopWordsList)
+            
     except IOError as e:
         print "Cannot read:",fileName
-    except:
-        print "Unexpected error"
 
 if __name__ == "__main__":
     main(sys.argv[1:])
