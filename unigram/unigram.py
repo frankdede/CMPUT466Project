e5e62bafe09e11b20bf7ea1d49f701d86e7d34d9
dyf102 <dyf102@gmail.com>
2014-11-13 23:13:28 -0700

    stupid bug fixed...

--- unigram/unigram.py
+++ unigram/unigram.py
@@ -4,13 +4,12 @@ def extractUnigrams(invertedRawData):
 	print(len(invertedRawData))
 	unigramCollection = {}
 	#print(invertedRawData[0])
-	for sentiment in invertedRawData:
+	counter = 0
+	for sentiment in invertedRawData.keys():
 		print("hello Processing sentiment " + str(sentiment) )
-        #if sentiment not in unigramCollection:
-        unigramCollection[str(sentiment)] = 'abc'
-        #for entry in invertedRawData[sentiment]:
- 		#	pass
-        	#unigramCollection[sentiment].extend(entry['sentence'])
-	for i in unigramCollection:
-		print i
+        	if sentiment not in unigramCollection:
+        		unigramCollection[sentiment] = []
+        	for entry in invertedRawData[sentiment]:
+        		unigramCollection[sentiment].extend(entry['sentence'])
+	print(len(unigramCollection.keys()))
 	return unigramCollection
