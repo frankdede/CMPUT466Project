498f35e16d8da324af352bb98bce6fc7a9a6211b
Tianyi Wu <tywtyw2002@gmail.com>
2014-11-25 22:48:54 -0700

    fix

--- decisionTree/find_best_accuracy.py
+++ decisionTree/find_best_accuracy.py
@@ -5,26 +5,25 @@
 
 import sys
 import argparse
-import buildtree as bt 
+import buildtree as bt
 
 
 def get_accuray_socre(t, pos, length, size):
     data_length = len(t.data)
-    end = pos + length 
-    
+    end = pos + length
+
     right = 0.0
     wrong_set = {}
-    index = t.attr['sentiment']['index'];
+    index = t.attr['sentiment']['index']
     for i in index:
         wrong_set[i] = {}
         for j in index:
             wrong_set[i][j] = 0
 
-
     for i in range(data_length):
         if i > pos and i < end:
             continue
-        row = t.data[i] 
+        row = t.data[i]
         r = t.classerify(row[:-1])
 
         real_r = str(row[-1])
@@ -35,16 +34,17 @@ def get_accuray_socre(t, pos, length, size):
 
         wrong_set[real_r][pref_r] += 1
 
-    accuracy = right / (data_length - length )
-    return (accuracy, wrong_set) 
+    accuracy = right / (data_length - length)
+    return (accuracy, wrong_set)
+
 
 def mk_test(t, freg_size):
-    data_length = len(t.data) 
-    
-    freg_count = data_length / freg_size 
+    data_length = len(t.data)
+
+    freg_count = data_length / freg_size
     pos = [freg_count * i for i in xrange(freg_size)]
-    
-    result = {'accuracy': [], 'error_set' : []} 
+
+    result = {'accuracy': [], 'error_set': []}
 
     for i in pos:
         t.fit_part(i, freg_count)
@@ -53,19 +53,20 @@ def mk_test(t, freg_size):
         result['error_set'].append(error_set)
 
     return result
-    
+
+
 def print_wrong_matrix(error_set):
-     
-    #print wrong predict keys
+
+    # print wrong predict keys
     print "\t\t",
     for i in sorted(error_set.keys()):
         print "%s\t\t" % i,
 
     print ""
 
-    #print error matrix
+    # print error matrix
     for row in sorted(error_set.keys()):
-        print "%s\t\t"%row,
+        print "%s\t\t" % row,
         error_col = error_set[row]
         for col in sorted(error_col.keys()):
             print "%s\t\t" % error_col[col],
@@ -80,28 +81,29 @@ def main():
         socre for different size of fregment.')
 
     parser.add_argument('INPUT', nargs=1, help="Input data file")
-    parser.add_argument("-d",dest='depth', default=-1 , help="max depth", type=int)    
+    parser.add_argument(
+        "-d", dest='depth', default=-1, help="max depth", type=int)
     args = parser.parse_args(sys.argv[1:])
-     
+
     t = bt.tree_builder(args.depth)
     t.load_data_from_file(args.INPUT[0])
 
     # freg_fit_result={}
     max_socre = 0
     max_socre_idx = -1
-    for i in xrange(2,21):
-         r = mk_test(t, i)
-         m = max(r['accuracy'])
-         index = r['accuracy'].index(m) + 1
-         print "\nsplit data into %02d fregment has high score %f with freg %d" % (i, m, index)
-         print_wrong_matrix(r['error_set'][index - 1]) 
-         
-         #freg_fit_result['i']['accuracy'] = m
-         #freg_fit_result['i']['index'] = index
-         #freg_fit_result['i']['freq'] = i
-         if m > max_socre:
+    for i in xrange(2, 21):
+        r = mk_test(t, i)
+        m = max(r['accuracy'])
+        index = r['accuracy'].index(m) + 1
+        print "\nsplit data into %02d fregment has high score %f with freg %d" % (i, m, index)
+        print_wrong_matrix(r['error_set'][index - 1])
+
+        #freg_fit_result['i']['accuracy'] = m
+        #freg_fit_result['i']['index'] = index
+        #freg_fit_result['i']['freq'] = i
+        if m > max_socre:
             max_socre = m
-            max_socre_idx = i 
+            max_socre_idx = i
             max_socre_index = index
 
     print "The high score accuracy is %s into %d block with index at %s" %\
